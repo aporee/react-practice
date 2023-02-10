ffa13a18c02ab29a759ca24fef9135e0a8d127db
k <ntleekor@gmail.com>
2023-02-10 11:20:21 +0900

    init

---
+++ src/component/Header.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { DarkContext } from '../context/DarkContext';
+
+const Header = () => {
+  const { isDark } = useContext(DarkContext);
+
+  return (
+    <div
+      className="header"
+      style={{
+        backgroundColor: isDark ? 'black' : 'white',
+        color: isDark ? 'white' : 'black',
+      }}
+    >
+      Welcome!
+    </div>
+  );
+};
+
+export default Header;
